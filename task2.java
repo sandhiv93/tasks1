589bd4390906cf2fe3f4b11517852aac1f13b057
sandhiv93 <167868273+sandhiv93@users.noreply.github.com>
2024-05-10 17:39:51 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,10 @@
+public class task2 {
+    public static void main(String[] args) {
+        String s ="Prepinsta";
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        System.out.println( "String is : "+sb);
+        sb.reverse();
+        System.out.println("Reversed string is : "+sb);
+    }
+}
